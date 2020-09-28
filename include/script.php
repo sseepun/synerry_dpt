ede32ad18809d50f5ab58bb2f092225e8ea5790c
Sarun Seepun <sarun_sla@hotmail.com>
2020-09-28 01:21:28 -0700

    Initialize structure

--- include/script.php
+++ include/script.php
@@ -1,7 +1,9 @@
-<script src="./assets/js/jquery-3.5.1.min.js"></script>
-<script src="./assets/lib/slick-1.8.1/slick/slick.min.js"></script>
-<script src="./assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.js"></script>
-<script src="./assets/lib/aos-2.3.1/aos.min.js"></script>
-<script src="./assets/js/script.js"></script>
+<script src="public/assets/node_modules/jquery/dist/jquery.min.js"></script>
+<script src="public/assets/node_modules/popper.js/dist/popper.min.js"></script>
+<script src="public/assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="public/assets/node_modules/slick-carousel/slick/slick.min.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js"></script>
+<script src="public/assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.js"></script>
+<script src="public/assets/lib/aos-2.3.1/aos.min.js"></script>
+
+<script src="public/assets/app/js/script.js"></script>
--- include/style.php
+++ include/style.php
@@ -9,21 +9,15 @@
     }
 </style>
 
-<link rel="stylesheet" href="./assets/fonts/includes.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+<link rel="stylesheet" href="public/assets/app/fonts/includes.css" />
+<link rel="stylesheet" href="public/assets/node_modules/@fortawesome/fontawesome-free/css/all.css" />
+<link rel="stylesheet" href="public/assets/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+<link rel="stylesheet" href="public/assets/node_modules/slick-carousel/slick/slick.css" />
 
-<link rel="stylesheet" href="./assets/lib/slick-1.8.1/slick/slick.css" />
-<link rel="stylesheet" href="./assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.css" />
-<link rel="stylesheet" href="./assets/lib/aos-2.3.1/aos.min.css" />
+<link rel="stylesheet" href="public/assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.css" />
+<link rel="stylesheet" href="public/assets/lib/aos-2.3.1/aos.min.css" />
 
-<link rel="stylesheet" href="./assets/css/style.css" />
-<link rel="stylesheet" href="./assets/css/grids.css" />
-
-<link rel="stylesheet" href="./assets/css/navs.css" />
-<link rel="stylesheet" href="./assets/css/blocks.css" />
-<link rel="stylesheet" id="css-theme" href="./assets/css/themes/0.css" />
-<link rel="stylesheet" href="./assets/css/ie-fix.css" />
-
-<link rel="stylesheet" href="./assets/css/custom-style.css" />
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css" />
+<link rel="stylesheet" href="public/assets/app/css/style.css" />
+<link rel="stylesheet" href="public/assets/app/css/navs.css" />
+<link rel="stylesheet" id="css-theme" href="public/assets/app/css/color-0.css" />
+<link rel="stylesheet" href="public/assets/app/css/ie-fix.css" />
