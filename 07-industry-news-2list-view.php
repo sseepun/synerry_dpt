44d65c7038e2908d688db58f51ee097566498886
theerapat pothisawang <tonn_po@hotmail.com>
2020-10-20 01:34:37 +0700

    07-industry-new-2list finished

--- 07-industry-news-2list-view.php
+++ 07-industry-news-2list-view.php
@@ -11,14 +11,15 @@
         .ss-card .h-line {margin-top:1rem; height: .25rem; background: #efefef; width:100%;}
         .text-container p {margin:0;}
         .text-container h3 {margin:0;}
-        .post-card .title-container .view {
+        /* .ss-card .date {margin: .125rem 0 .5rem 0;} */
+        .ss-card .view {
             font-family: 'TH Sarabun New';
-            margin: .125rem 0 .5rem 0;
+            margin: .5rem 0 0 .25rem;
             color: #88009b;
         }
-        .post-card .title-container .view .icon {
+        .ss-card .view .icon {
             width:1.5rem; height:1.5rem; display:block;
-            font-size: .9375rem; margin: 0 .5rem 0 0; text-align:center; line-height:1.75rem;
+            font-size: .9375rem; margin: 0 .25rem 0 0; text-align:center; line-height:1.75rem;
         }
         .slots {margin-top:-5.5rem;}
         .slot {
@@ -97,70 +98,56 @@
                 <div class="grid lg-70 md-70 sm-100">
                     <div class="grids">
                         <?php for($i = 0; $i < 9; $i++){?>
-                            <div class="grid lg-1-3 md-50 sm-50">
-                                <div class="ss-card mb-3">
-                                    <div class="block w-auto">
-                                        
-                                        <div class="post-card">
-                                            <div class="title-container">
-                                                <a class="title h4" href="#">
-                                                    รมว.สุริยะฯ เผย นิสสันมั่นใจศักยภาพฐานการผลิตของไทย
-                                                </a>
-                                                <div class="d-flex jc-space-between">
-                                                    <div class="date" style="color: #88009b;">13.08.2563</div>
-                                                    <div class="d-flex view"><div class="icon"><i class="fas fa-eye"></i></div>268</div>
-                                                </div>
-                                            </div>
-                                            <div class="ss-img square bradius-0">
-                                                <div class="img-bg lazy-bg" data-src="public/assets/app/images/banner/0<?= ($i%7)+1; ?>.jpg"></div>
-                                                <div class="socials">
-                                                    <ul>
-                                                        <li><a class="social social-fw" href="#">
-                                                            <i class="fab fa-facebook-f"></i>
-                                                        </a></li>
-                                                        <li><a class="social social-tw" href="#">
-                                                            <i class="fab fa-twitter"></i>
-                                                        </a></li>
-                                                        <li><a class="social social-ln" href="#">
-                                                            <i class="fab fa-line"></i>
-                                                        </a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <p class="desc">
-                                                กระทรวงอุตสาหกรรม (อก.) ผลักดันแผน
-                                                อุตสาหกรรมเครื่องจักรกลให้เป็นอุตสาหกรรม
-                                                สนับสนุนเพิ่มขีดความสามารถการผลิต...
-                                            </p>
-                                            <div class="btns">
-                                                <a class="btn-action btn-action-primary" href="#">
-                                                    <i class="fas fa-chevron-right"></i>
-                                                    อ่านรายละเอียด
-                                                </a>
-                                            </div>
+                            <div class="ss-card mb-3">
+                                <div class="block">
+                                    <div class="ss-img square bradius-0">
+                                        <div class="img-bg lazy-bg" data-src="public/assets/app/images/banner/0<?= ($i%7) +1; ?>.jpg"></div>
+                                        <div class="socials">
+                                            <ul>
+                                                <li><a class="social social-fw" href="#">
+                                                    <i class="fab fa-facebook-f"></i>
+                                                </a></li>
+                                                <li><a class="social social-tw" href="#">
+                                                    <i class="fab fa-twitter"></i>
+                                                </a></li>
+                                                <li><a class="social social-ln" href="#">
+                                                    <i class="fab fa-line"></i>
+                                                </a></li>
+                                            </ul>
                                         </div>
-                                        <div class="h-line"></div>
-
                                     </div>
+                                </div>
+                                <div class="block">
+                                    <div class="post-card post-card-04">
+                                        <div class="title-container">
+                                            <a class="title h4" href="#">
+                                                สุริยะฯ ดันแผนพัฒนาอุตฯ 
+                                                เครื่องจักรกลเต็มสูบ มุ่งสนับสนุน
+                                                เกษตรแปรรูป 
+                                            </a>
+                                        </div>
+                                        <div class="d-flex">
+                                            <div class="date" style="color: #88009b;">13.08.2563</div>
+                                            <div class="d-flex view"><div class="icon"><i class="fas fa-eye"></i></div>268</div>
+                                        </div>
+                                        <p class="desc">
+                                            กระทรวงอุตสาหกรรม (อก.) ผลักดันแผน
+                                            อุตสาหกรรมเครื่องจักรกลให้เป็นอุตสาหกรรม
+                                            สนับสนุนเพิ่มขีดความสามารถการผลิต...
+                                        </p>
+                                    </div>
+                                    <div class="btns">
+                                        <a class="btn-action btn-action-primary" href="#">
+                                            <i class="fas fa-chevron-right"></i>
+                                            อ่านรายละเอียด
+                                        </a>
+                                    </div>
+                                    <div class="h-line"></div>
                                 </div>
                             </div>
                         <?php }?>
                     </div>
 
-                    <!-- <div class="pagination mt-5" data-aos="fade-up" data-aos-delay="0">
-                        <div class="wrapper">
-                            <a href="#" class="page-btn page-prev disabled"></a>
-                            <a href="#" class="page-btn active">01</a>
-                            <a href="#" class="page-btn">02</a>
-                            <a href="#" class="page-btn">03</a>
-                            <a href="#" class="page-btn">04</a>
-                            <a href="#" class="page-btn">05</a>
-                            <a href="#" class="page-btn page-next"></a>
-                            <a class="btn btn-round custom-btn-dark btn-sm" href="#">
-                                อ่านข่าวทั้งหมด
-                            </a>
-                        </div>
-                    </div> -->
                     <div class="list-footer">
                         <div class="block">
                             <div class="text-wrapper text-sm">
