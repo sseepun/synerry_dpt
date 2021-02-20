adfc525925f11b425ebbb3872f5e375f201c388b
sseepun <sarun_sla@hotmail.com>
2021-02-21 02:41:12 +0700

    Complete 16-rss

--- component/list-footer.php
+++ component/list-footer.php
@@ -1,27 +1,31 @@
 <div class="list-footer">
     <div class="block">
-        <div class="text-wrapper text-sm">
-            จำนวนทั้งหมด <span class="fw-600 color-07">400 รายการ</span> 
+        <div class="text-wrapper color-gray text-sm">
+            จำนวนทั้งหมด <span class="fw-600 color-01">400 รายการ</span> 
         </div>
     </div>
     <div class="block">
         <div class="pagination">
             <div class="wrapper">
-                <a href="#" class="page-btn page-prev disabled"></a>
+                <a href="#" class="page-btn page-prev disabled">
+                    <em class="zmdi zmdi-arrow-left"></em>
+                </a>
                 <a href="#" class="page-btn active">01</a>
                 <a href="#" class="page-btn">02</a>
                 <a href="#" class="page-btn">03</a>
                 <a href="#" class="page-btn">04</a>
                 <a href="#" class="page-btn">05</a>
-                <a href="#" class="page-btn page-next"></a>
+                <a href="#" class="page-btn page-next">
+                    <em class="zmdi zmdi-arrow-right"></em>
+                </a>
             </div>
         </div>
     </div>
     <div class="block">
-        <div class="text-wrapper text-sm">
+        <div class="text-wrapper color-gray text-sm">
             แสดงผลต่อหน้า
             <div class="select-wrapper">
-                <select class="pp">
+                <select class="pp color-01 fw-600 round">
                     <option value="10">10</option>
                     <option value="25">25</option>
                     <option value="50">50</option>
--- include/style.php
+++ include/style.php
@@ -14,5 +14,5 @@
 <link rel="stylesheet" href="public/assets/app/css/style.css" />
 <link rel="stylesheet" href="public/assets/app/css/navs.css" />
 <link rel="stylesheet" href="public/assets/app/css/blocks.css" />
-<link rel="stylesheet" id="css-theme" href="public/assets/app/css/color-0.css" />
+<link rel="stylesheet" id="css-theme" href="public/assets/app/css/color-1.css" />
 <link rel="stylesheet" href="public/assets/app/css/ie-fix.css" />
