39764e6cf4c37d9fae3c49313f153f494ca5c932
sseepun <sarun_sla@hotmail.com>
2021-06-05 00:55:24 +0700

    Add TODO pages

---
+++ TODO-minisite-13-career-list.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <?php include_once('include/header.php'); ?>
+    <?php include_once('include/style.php'); ?>
+</head>
+<body class="loading">
+    <?php 
+        $topnavActiveIndex = 1;
+        include_once('include/topnav-minisite.php');
+    ?>
+    <?php
+        $breadcrumb = [
+            'leading' => 'แ',
+            'title' => 'แผนผังองค์กร',
+            'desc' => 'กรมโยธาธิการและผังเมืองพร้อมให้ข้อมูลข่าวสารที่เป็นประโยชน์ สำหรับคุณ',
+            'image' => 'public/assets/app/images/bg/minisite-breadcrumb-04.jpg',
+            'structure' => [
+                [ 'name' => 'แนะนำกรม', 'url' => '#' ],
+                [ 'name' => 'แผนผังองค์กร', 'url' => '#' ]
+            ]
+        ];
+        include_once('component/breadcrumb-02.php');
+    ?>
+    
+    <section class="section-container" data-aos="fade-up" data-aos-delay="300">
+        <div class="container">
+
+            
+
+        </div>
+    </section>
+
+    <?php include_once('include/footer-minisite.php'); ?>
+    <?php include_once('include/script.php'); ?>
+</body>
+</html>
